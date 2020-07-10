9cf0b022d1d169096ed915415bea84bd88e2f102
underd0g <underd0g@underd0gs-MacBook-Air.local>
2020-07-09 23:41:52 -0400

    added the server definitions

---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+var express = require('express');
+var app = express();
+var body-parser = require('body-parser');
+var fetch = require('node-fetch');
+
+//add the index route to handle requests to the main page
+
+app.get('/', function(request,request){
+console.log('hit the index page');
+});
+
+//start the server on port 3000 of the host. 
+
+app.listen('3000', function(){
+console.log('listening on port 3000... and localhost');
+});
+
+
