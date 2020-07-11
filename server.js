f9921aa4b75b8a002e5a468f3e716b8ca269f7a8
underd0g1 <hide4@comcast.net>
2020-07-10 22:16:41 -0400

    fixed body-parser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var body-parser = require('body-parser');
+var bodyparser = require('body-parser');
 var fetch = require('node-fetch');
 
 //add the index route to handle requests to the main page
@@ -11,10 +11,8 @@ app.get('/', function(request,request){
 console.log('hit the index page');
 });
 
-//start the server on port 3000 of the host. 
+//start the server on port 3000 of the host.
 
 app.listen('3000', function(){
 console.log('listening on port 3000... and localhost');
 });
-
-
